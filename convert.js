425d549e36ab10f73b95aff9298e32b86d20eb74
SimonPrimetzhofer <31982146+SimonPrimetzhofer@users.noreply.github.com>
2017-09-21 10:36:51 +0200

    Add files via upload

--- convert.js
+++ convert.js
@@ -1,6 +1,6 @@
 var arg=process.argv;
 var converter=require('convert-units');
-if(arg.length!=6||isNaN(arg[2])||arg.indexOf('to')==-1||converter().possibilities().indexOf(`${arg[3]}`)==-1||converter().possibilities().indexOf(`${arg[5]}`)==-1)
+if(arg.length!=6||isNaN(arg[2])||arg.indexOf('to')==-1||converter().possibilities().indexOf(`${arg[3]}`)==-1||converter().possibilities().indexOf(`${arg[5]}`)==-1||converter().from(`${arg[3]}`).possibilities().indexOf(`${arg[5]}`)==-1)
     failure();
 console.log(`${arg[2]} ${arg[3]} are `+converter(`${arg[2]}`).from(`${arg[3]}`).to(`${arg[5]}`)+` ${arg[5]}`);
 function failure(){
