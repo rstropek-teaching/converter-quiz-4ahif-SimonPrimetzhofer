2fd808ca647f2915e0cdabd62aace296ac821e6e
SimonPrimetzhofer <31982146+SimonPrimetzhofer@users.noreply.github.com>
2017-09-21 10:29:24 +0200

    Add files via upload

--- convert.js
+++ convert.js
@@ -1,1 +1,9 @@
-console.log('Not implemented yet!');
+var arg=process.argv;
+var converter=require('convert-units');
+if(arg.length!=6||isNaN(arg[2])||arg.indexOf('to')==-1||converter().possibilities().indexOf(`${arg[3]}`)==-1||converter().possibilities().indexOf(`${arg[5]}`)==-1)
+    failure();
+console.log(`${arg[2]} ${arg[3]} are `+converter(`${arg[2]}`).from(`${arg[3]}`).to(`${arg[5]}`)+` ${arg[5]}`);
+function failure(){
+    console.log("Invalid parameters");
+    process.exit(-1);
+}
